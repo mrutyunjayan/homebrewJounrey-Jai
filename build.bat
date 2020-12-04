14370365448e638d44a8d6e684515229e290196b
mrutyunjayan <jaikrishnanarun@hotmail.com>
2020-12-04 11:00:34 -0800

    001

--- build.bat
+++ build.bat
@@ -1,7 +1,75 @@
-@echo off
+@ECHO OFF
 
-set opts=-FC -GR- -EHa- -Od -Oi -MT -TC -Wall -WX -Wno-unused-parameter -Wno-nonportable-system-include-path -nologo -Z7
-set code=%cd%
-pushd build
-clang-cl %opts% %code%\code\main.c -FehomebrewJourney
-popd
+IF NOT DEFINED DevEnvDir (
+	call "J:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64 >nul
+)
+
+SET debug_flags=^
+	-Zi^
+	-Od^
+	-MTd^
+	-DEBUG
+
+SET compiler_flags=^
+	-std:c11^
+	-nologo^
+	-FC^
+	-fp:fast^
+	-Gm-^
+	-GR-^
+	-EHsc-^
+	-Oi^
+	-WX^
+	-W4^
+	-wd4201^
+	-wd4100^
+	-wd4189^
+	-wd4505^
+	-wd4068^
+	%debug_flags%
+
+SET defines=^
+	-DHOMEBREW_DEBUG^
+	-D_CRT_SECURE_NO_WARNINGS^
+	-DHOMEBREW_CONSOLE
+
+SET libs=^
+	user32.lib^
+	gdi32.lib^
+	winmm.lib^
+	Kernel32.lib^
+	vulkan-1.lib
+
+SET linker_flags=^
+	-link^
+	-LIBPATH:%VULKAN_SDK%\Lib^
+	-incremental:no^
+	-opt:ref^
+	-SUBSYSTEM:CONSOLE^
+	%libs%
+
+SET project=%cd%
+
+SET include_directories=^
+	-I %project%\include^
+	-I %VULKAN_SDK%\Include
+
+IF NOT EXIST build mkdir build
+
+PUSHD build
+
+cl^
+	%defines%^
+	%include_directories%^
+	%compiler_flags%^
+	%project%\code\main.c^
+	-Fehomebrew_journey^
+	%linker_flags%
+
+POPD
+
+IF %ERRORLEVEL% NEQ 0  (
+	ECHO 	   ===============
+	ECHO Error:	%ERRORLEVEL%
+	ECHO 	   ================
+)
---
+++ code/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include <vulkan/vulkan.h>
+
+int 
+main(int argc, char** argv) {
+	
+	printf("Test\n");
+	
+	return ;
+}
